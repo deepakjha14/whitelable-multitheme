ce44f25c29779c1a8bf994b23cfdbe461fef9e50
Deepak Jha <deepakjha14.ex@gmail.com>
2024-11-09 21:52:12 +0530

    Theme in angular 18 done with the use of variables

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,6 +3,8 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { StudentsTableComponent } from '../students-table/students-table.component';
 import { StudentRecordsComponent } from "../student-records/student-records.component";
 import { ContactUsComponent } from '../contact-us/contact-us.component';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,10 +13,26 @@ import { ContactUsComponent } from '../contact-us/contact-us.component';
     MatTabsModule,
     StudentsTableComponent,
     StudentRecordsComponent,
-    ContactUsComponent
+    ContactUsComponent,
+    MatButtonToggleModule,
+    CommonModule
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+
+  theme: string = "default";
+
+  onDefaultTheme() {
+    // Set the default theme
+    document.getElementsByTagName("body")[0].className = "";
+    this.theme = "default";
+  }
+
+  onDarkTheme() {
+    // Set the default theme
+    document.getElementsByTagName("body")[0].className = "dark-theme";
+    this.theme = "dark";
+  }
 }
